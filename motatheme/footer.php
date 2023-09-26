d56d08db0df804fc0a94223c5fa8ffed36f5716f
sebbastien39 <sebbastien39@gmail.com>
2023-09-26 17:39:40 +0200

    Mise en forme CSS header.php footer.php

--- motatheme/footer.php
+++ motatheme/footer.php
@@ -1,10 +1,12 @@
 <!--Pied de page appelé sur toutes les pages du site-->
 <?php get_template_part('template-parts/modale'); ?>
 <?php wp_footer() //Affiche WP action, ligne du dashboard?>
-<?php wp_nav_menu([
+<footer>
+    <?php wp_nav_menu([
             'theme_location' => 'footer-menu',
-            'container' => 'p',
+            //'container' => 'footer',
             ]); //Affiche le menu créé ds functions.php?>
-<p>TOUS DROITS RÉSERVÉS</p>
+    <p>TOUS DROITS RÉSERVÉS</p>
+</footer>
 </body>
 </html>
